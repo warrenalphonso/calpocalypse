9653f3d3c77bab47123802367f29928681d8ac5b
Warren Alphonso <warrenalphonso02@gmail.com>
2019-05-28 21:41:47 -0700

    heroku up and running

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Index = () => (
       <h3>This is the menu page</h3>
       <Link href='/story'><h4>Story</h4></Link>
       <Link href='/controls'><h4>Controls</h4></Link>
-      <Link prefetch href='/characters'><h4>Characters</h4></Link>
+      <Link href='/characters'><h4>Characters</h4></Link>
       <Link prefetch href='/game'><h4>Game</h4></Link>
     </div>
 )
