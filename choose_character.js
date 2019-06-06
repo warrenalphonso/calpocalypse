7cd8b5485a7c54e3abdf18b84f930dcc06302ea7
Warren Alphonso <warrenalphonso02@gmail.com>
2019-06-05 17:40:19 -0700

    char choosing screen works and sets state of App

--- choose_character.js
+++ choose_character.js
@@ -16,6 +16,10 @@ class ChooseCharacter extends Component {
         console.log(`clicked: state is ${e.currentTarget.value}`)
     }
 
+    handleChange = (e) => {
+        this.props.onChooseCharacter(e.target.value)
+    }
+
     render() {
         return (
             <div>
@@ -24,13 +28,14 @@ class ChooseCharacter extends Component {
                 </Head>
                 <h2>Choose a character</h2>
                 
-                <input type="radio" name="character" value="eecs" onChange={this.radioClicked} /> EECS <br />
-                <input type="radio" name="character" value="mcb" onChange={this.radioClicked} /> MCB <br />
-                <input type="radio" name="character" value="econ" onChange={this.radioClicked} /> Econ <br />
-                <input type="radio" name="character" value="ms" onChange={this.radioClicked} /> Media Studies <br />
+                <input type="radio" name="character" value="eecs" onChange={this.handleChange} /> EECS <br />
+                <input type="radio" name="character" value="mcb" onChange={this.handleChange} /> MCB <br />
+                <input type="radio" name="character" value="econ" onChange={this.handleChange} /> Econ <br />
+                <input type="radio" name="character" value="ms" onChange={this.handleChange} /> Media Studies <br />
 
             </div>
         )
+
     }
 }
 
--- pages/game.js
+++ pages/game.js
@@ -7,6 +7,29 @@ import Game from '../green_dot_test'
 import ChooseCharacter from '../choose_character'
 //todo: convert class to functions, choose a character then screen changes to the game. 
 
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            character: null
+        }
+    }
+
+    onChooseCharacter = (char) => {
+        this.setState({
+            character: char
+        })
+    }
+
+    render() {
+        if (this.state.character == null) {
+            return <ChooseCharacter onChooseCharacter = {this.onChooseCharacter} />
+        } else {
+            return <Game />
+        }
+    }
+
+}
   
 
-export default ChooseCharacter
+export default App
