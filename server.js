4c7d583f25afbbe5508b98ac05f9a66d62423109
Warren Alphonso <warrenalphonso02@gmail.com>
2019-08-05 20:45:28 -0700

    testing JSON

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}.`));
 
 app.get('/api', (req, res) => {
-  res.status(200).json({api: 'version 1'});
+  res.status(200).json({api: 'version 1', blah: 'yo', numbers: 235});
 });
