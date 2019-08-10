17c5cd4f01857d759cd3d74d02a9c3a2de8e3cf5
Warren Alphonso <warrenalphonso02@gmail.com>
2019-08-10 06:15:37 -0700

    testing movement rendering

--- js/paperjs/tiles.js
+++ js/paperjs/tiles.js
@@ -1,11 +1,8 @@
-// var p = new Path.Rectangle({width: 100, height: 100})
-// var fillColors = ['black', 'red', 'blue', 'green']
-// var index = 0, count = 0
-
 update();
+
 function update(){
     $.getJSON('https://cors-anywhere.herokuapp.com/https://calpocalypse-backend.herokuapp.com/blocks', function(data){
-		blocks = data.blocks
+		blocks = data
 		for (var y = 0; y < blocks.length; y++) {
 			for (var x = 0; x < blocks[y].length; x++) {
 				var block = new Path.Rectangle(50* x, 50 * y, 50, 50)
@@ -16,11 +13,6 @@ function update(){
 				}
 			}
 		}
-		setTimeout(update, 1000)
-        // index = data.currentFileTime % 1013 % fillColors.length
-        // p.fillColor = fillColors[index]
-        // if(++count < 20){
-        //     setTimeout(update, 1500)
-        // }
+		setTimeout(update, 1000 / 60)
     })
 }
--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server started on port ${port}.`))
 
 
+var originalBlocks = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,2,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
+
 var blocks = [
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   [1,2,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -21,19 +35,22 @@ var blocks = [
   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
-var i = 2
-
 var players = {}
 
 const genStartCoords = () => {
-  return [0, 0]
+  return [1, 1]
+}
+
+const movePlayer = (id, newX, newY) {
+  blocks[players[id].y][players[id].x] = originalBlocks[players[id].y][players[id].x]
+  players[id].x = newX
+  players[id].y = newY
+  blocks[newY][newX] = 2
 }
 
 // GET blocks 
 app.get('/blocks', (req, res) => {
   res.status(200).json(blocks)
-  blocks[1][i] = 2
-  i++
 })
 
 // GET all players
@@ -44,14 +61,13 @@ app.get('/players', (req, res) => {
 // POST new player 
 app.post('/players/:name/:char', (req, res) => {
   const id = uniqid()
-  const startCoords = genStartCoords()
   players[id] = {
     name: req.params.name,
     facing: 'n',
-    char: req.params.char, 
-    x: startCoords[0], 
-    y: startCoords[1]
+    char: req.params.char
   }
+  const startCoords = genStartCoords()
+  movePlayer(id, startCoords[0], startCoords[1])
   // Sends back ID 
   res.status(200).json({
     id: id
@@ -66,7 +82,6 @@ app.delete('/players/:id', (req, res) => {
 
 // PATCH player position 
 app.patch('/players/:id/:dx/:dy', (req, res) => {
-  players[req.params.id].x += parseInt(req.params.dx, 10)
-  players[req.params.id].y += parseInt(req.params.dy, 10)
+  movePlayer(req.params.id, parseInt(req.params.dx, 10), parseInt(req.params.dy, 10))
   res.status(200).send(`PATCH request for id ${req.params.id} with dx: ${req.params.dx} and dy: ${req.params.dy} was successful.`)
 })
